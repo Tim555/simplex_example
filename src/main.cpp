95bd32ece57539a5ae2bffa75f088036417fae43
Tim Leys <tim.leys@uantwerpen.be>
2026-09-21 09:23:34 +0200

    main now reads from file

--- src/main.cpp
+++ src/main.cpp
@@ -1,27 +1,22 @@
+#include "lp/lp_parser.hpp"
 #include "lp/solver.hpp"
 #include <Eigen/Dense>
 #include <iostream>
 
-int main() {
-    const Eigen::MatrixXd inequalities{{1.0, 1.0}, {-1.0, -1.0}};
-    Eigen::VectorXd inequalities_rhs(2);
-    inequalities_rhs << 1.0, -3.0;
-    Eigen::VectorXd maximization_function(2);
-    maximization_function << 1.0, 1.0;
+int main(int argc, char *argv[]) {
+    if (argc != 2) {
+        std::cerr << "usage: " << argv[0] << " <lp-file.yaml>" << std::endl;
+        return 1;
+    }
 
-    // const Eigen::MatrixXd inequalities{{1.0, 2.0}, {2.0, 1.0}};
-    // Eigen::VectorXd inequalities_rhs(2);
-    // inequalities_rhs << 6.0, 8.0;
-    // Eigen::VectorXd maximization_function(2);
-    // maximization_function << 3.0, 2.0;
-
-    NaturalLinearProgram linear_program(
-        inequalities, inequalities_rhs, maximization_function,
-        NaturalLinearProgram::Sense::Minimize,
-        NaturalLinearProgram::Domain::Natural);
-    SimplexSolver simplex(linear_program);
-
-    std::cout << simplex.solve() << std::endl;
+    try {
+        const std::shared_ptr<LinearProgram> linear_program = LPParser::parse(argv[1]);
+        SimplexSolver simplex(*linear_program);
+        std::cout << simplex.solve() << std::endl;
+    } catch (const std::exception &e) {
+        std::cerr << "error: " << e.what() << std::endl;
+        return 1;
+    }
 
     return 0;
 }
